6fb888cf3f5055b1935ca714dc747f65dcb857f8
Charles Sneedworth <sneedworth@sevensevenseven.net>
2025-02-20 21:16:08 +0100

    working simon game

--- main.gd
+++ main.gd
@@ -19,15 +19,18 @@ func _process(delta):
 		time_remaining -= delta
 		minigame_time -= delta
 		global.total_time += delta
-		if mini_timer > 0 and (minigame_type == 1 or (minigame_type == 2 and simon_timer_freeze == false)):
-			mini_timer = max(0, mini_timer - delta )
+		if mini_timer > 0:
+			if simon_freeze == false:
+				mini_timer = max(0, mini_timer - delta )
 			if minigame_type == 1:
 				$Mini_Display/Mini_Label.text = format_seconds(mini_timer)
 				$Time_Display/Small_Time_Label.text = (format_time(time_remaining))
 				_math_game()
-			elif minigame_type == 2:
+			elif minigame_type == 0:
 				$Mini_Display/Mini_Label.text = format_seconds(mini_timer)
-				$Pin_Display/Pin_Label.text = "BOMB SAYS"
+				$Time_Display/Small_Time_Label.text = (format_time(time_remaining))
+				if simon_wait == true:
+					_simon_guess()
 		elif minigame_time < 0 and minigame_on == false:
 			minigame_on = true
 			_minigame()
@@ -69,7 +72,7 @@ var mini_win : bool = true
 
 func _minigame():
 	if minigame_type == null:
-		minigame_type = randi_range(1,1) #increase range for more minigame types
+		minigame_type = randi_range(0,0) #increase range for more minigame types
 		print(minigame_type)
 		mini_timer = 10
 		minigame[minigame_type].call()
@@ -218,36 +221,103 @@ func _on_button_minus_pressed() -> void:
 var simon_game_on: bool = false
 var simon_color: int 
 var simon_order: String # Combined String of generated colors
+var simon_pause: bool = false
 var simon_wait: bool #Stop picking colors, wait for input
 var simon_guess: String #User-Entered String to compare against simon_order
 var simon_continue: bool = true#Select a color after this one? Disabled by picking 5th rand option
-var simon_timer_freeze: bool #Freeze timer while game generates colors
+var simon_freeze: bool = false
 
 func _simon_game():
-	pass
+	simon_wait = false
+	simon_freeze = true
+	$Pin_Display/Pin_Label.text = "SIMON"
+	_simon_generator()
 	
 func _simon_generator():
-	simon_color = randi_range(0, 4)
-	match simon_color:
-		0: #Blue
-			simon_order += "0"
-			$Simon_Game/SimonBlue/SimonBlueLight.z_index = 2
-		1: #Greens
-			simon_order += "1"
-			$Simon_Game/SimonGreen/SimonGreen.z_index = 2
-		2: #Red
-			simon_order += "2"
-			$Simon_Game/SimonRed/SimonRed.z_index = 2
-		3: #Yellow
-			simon_order += "3"
-			$Simon_Game/SimonYellow/SimonYellowLight.z_index = 2
-		4: #TERMINATE
-			simon_continue = false
-	$Simon_Game/Simon_Timer.start(1.0)
+	if simon_order == "":
+		simon_color = randi_range(0, 3)
+	else:
+		simon_color = randi_range(0, 4)
+	if simon_pause == false:
+		match simon_color:
+			0: #Blue
+				simon_order += "0"
+				$Simon_Game/SimonBlue/SimonBlueLight.z_index = 2
+				$Time_Display/Time_Label.text = "BLUE"
+			1: #Greens
+				simon_order += "1"
+				$Simon_Game/SimonGreen/SimonGreenLight.z_index = 2
+				$Time_Display/Time_Label.text = "GREEN"
+			2: #Red
+				simon_order += "2"
+				$Simon_Game/SimonRed/SimonRedLight.z_index = 2
+				$Time_Display/Time_Label.text = "RED"
+			3: #Yellow
+				simon_order += "3"
+				$Simon_Game/SimonYellow/SimonYellowLight.z_index = 2
+				$Time_Display/Time_Label.text = "YELLOW"
+			4: #TERMINATE
+				simon_continue = false
+				$Time_Display/Time_Label.text = "ENTER"
+		simon_pause = true
+		$Simon_Game/Simon_Timer2.start(0.5)
 
-
+func _on_simon_timer_2_timeout() -> void:
+	_simon_color_reset()
+	$Simon_Game/Simon_Timer.start(0.5)
+	
 func _on_simon_timer_timeout() -> void:
-	$Simon_Game/SimonBlue/SimonBlueLight.z_index = 0
-	$Simon_Game/SimonGreen/SimonGreen.z_index = 0
-	$Simon_Game/SimonRed/SimonRed.z_index = 0
-	$Simon_Game/SimonYellow/SimonYellowLight.z_index = 0
+	simon_pause = false
+	if simon_continue == true:
+		_simon_generator()
+	else:
+		mini_timer = 10
+		simon_wait = true
+		simon_freeze = false
+func _simon_color_reset():
+	$Simon_Game/SimonBlue/SimonBlueLight.z_index = -1
+	$Simon_Game/SimonGreen/SimonGreenLight.z_index = -1
+	$Simon_Game/SimonRed/SimonRedLight.z_index = -1
+	$Simon_Game/SimonYellow/SimonYellowLight.z_index = -1	
+	
+func _simon_guess(): #Check if entered combination matches
+	if simon_guess == simon_order:
+		mini_win = true
+		_simon_reset()
+	elif mini_timer == 0 or simon_guess.length() > simon_order.length():
+		mini_win = false
+		_simon_reset()
+		
+func _simon_reset():
+	_simon_color_reset()
+	simon_game_on = false
+	simon_order = ""# Combined String of generated colors
+	simon_pause = false
+	simon_wait = false#Stop picking colors, wait for input
+	simon_guess = "" #User-Entered String to compare against simon_order
+	simon_continue = true#Select a color after this one? Disabled by picking 5th rand option
+	_mini_reset()
+
+func _on_ss_button_b_pressed() -> void:
+	if simon_wait == true:
+		simon_guess += "0"
+		_simon_color_reset()
+		$Simon_Game/SimonBlue/SimonBlueLight.z_index = 2
+		
+func _on_ss_button_g_pressed() -> void:
+	if simon_wait == true:
+		simon_guess += "1"
+		_simon_color_reset()
+		$Simon_Game/SimonGreen/SimonGreenLight.z_index = 2
+		
+func _on_ss_button_r_pressed() -> void:
+	if simon_wait == true:
+		simon_guess += "2"
+		_simon_color_reset()
+		$Simon_Game/SimonRed/SimonRedLight.z_index = 2
+		
+func _on_ss_button_y_pressed() -> void:
+	if simon_wait == true:
+		simon_guess += "3"
+		_simon_color_reset()
+		$Simon_Game/SimonYellow/SimonYellowLight.z_index = 2
